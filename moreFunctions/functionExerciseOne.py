122fd916da5c91bf9beaaef2d3364cb1bfcda11a
Indigo Emanuel <indigo.emanue@gmail.com>
2020-02-13 10:06:08 -0500

    Made a Celcius Calculator

--- moreFunctions/functionExerciseOne.py
+++ moreFunctions/functionExerciseOne.py
@@ -4,4 +4,13 @@
 # Returning Values
 
 
-#Double Function
+#Celcius to Farenheight Calculator
+
+print("Enter a Celcius Value of Tempurature")
+Celcius = input()
+
+celToFar = int(Celcius) * 9/5 + 32
+
+print (f'{Celcius} degrees Celcius is equivalent to {celToFar} degrees Farenheight')
+
+
